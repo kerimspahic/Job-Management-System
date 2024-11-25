5f7d2727d9cf514e0823946125cc0ef1c9f37615
Kerim Spahic <kerimspahic63@gmail.com>
2024-11-25 21:42:24 +0100

    Email-based Authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  helper_method :logged_in?, :current_user
+
+  def current_user
+    @current_user ||= lookup_user_by_cookie
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  private
+
+  def lookup_user_by_cookie
+    if cookies.permanent.encrypted[:access_token]
+      User.joins(:access_tokens).find_by(
+        access_tokens: {
+          token: cookies.permanent.encrypted[:access_token]
+        }
+      )
+    end
+  end
 end
